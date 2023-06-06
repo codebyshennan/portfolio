18b8b7f1f4ffb74df9bf1cefa864759713a75e0b
Shen Nan <wongshennan@gmail.com>
2023-06-06 14:42:09 +0800

    fix: og and favicon

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -24,7 +24,7 @@ export default async function handler(req: NextRequest) {
           flexDirection: "column",
           alignItems: "flex-start",
           justifyContent: "center",
-          backgroundImage: "url(https://byshennan.com/og-bg.png)",
+          backgroundImage: "url(https://byshennan.com/og.png)",
         }}
       >
         <div
